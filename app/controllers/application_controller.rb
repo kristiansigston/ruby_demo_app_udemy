1554f28b292ca76ed927669dcd4b99419b6dc786
Kristian Sigston <kristians@reallyenglish.com>
2021-01-11 21:02:49 +0000

    fix(users): add userrs pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  def gravatar_for(user)
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}"
+    image_tag(gravatar_url, alt: user.username)
+  end
 
+  def random_image_for(options)
+    url = "<img src='https://source.unsplash.com/random/200x200' />"
+    url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  def index
+    @users = User.all
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
